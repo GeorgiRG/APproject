db832ffbe8f7f0c81d7a3fdf49a1b38798ff7d65
GeorgiRG <grgeorgi93@gmail.com>
2020-11-19 22:01:40 +0200

    +

---
+++ schemas/user.py
@@ -0,0 +1,22 @@
+from marshmallow import fields, Schema
+
+from utils import hash_password
+
+
+class UserSchema(Schema):
+    class Meta:
+        ordered = True
+
+    id = fields.Int(dump_only=True)
+    username = fields.String(required=True)
+    email = fields.Email(required=True)
+    password = fields.Method(required=True, deserialize='load_password')
+    is_turkuamk = fields.Boolean(dump_only=True)
+    is_admin = fields.Boolean(dump_only=True)
+
+
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+
+    def load_password(self, value):
+        return hash_password(value)
